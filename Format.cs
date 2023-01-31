750562a06deea5637cad95556eceb78cd5c2d1e9
James John McGuire <jamesjohnmcguire@gmail.com>
2023-01-31 19:37:02 +0900

    Give Format it's Own File

---
+++ Format.cs
@@ -0,0 +1,24 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="Format.cs" company="James John McGuire">
+// Copyright © 2006 - 2023 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.Common.OfficeHelper
+{
+	public enum Format
+	{
+		/// <summary>
+		/// Date format.
+		/// </summary>
+		Date,
+		/// <summary>
+		/// General format.
+		/// </summary>
+		General,
+		/// <summary>
+		/// Text format.
+		/// </summary>
+		Text
+	}
+}
